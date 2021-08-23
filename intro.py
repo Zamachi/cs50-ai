95ade80613531a3c97b59f0f8365e503385d1b46
Zamachi <dstefa.dimitrijevic7@gmail.com>
2021-08-23 20:12:27 +0200

    First commit

---
+++ intro.py
@@ -0,0 +1,11 @@
+def fibonacci(number):
+
+    if(number == 0):
+        return 0
+    elif(number == 1 or number == 2):
+        return 1
+    else:
+        return fibonacci(number-1)+fibonacci(number-2)
+
+broj = fibonacci(9)
+print (broj)
